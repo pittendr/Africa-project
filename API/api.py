d4ed983dcea00555f8e644d92558c01c577c6ddc
nima <nima@crop-sense.com>
2016-03-18 17:45:29 -0400

    fixed latitude, longitude bug

--- API/api.py
+++ API/api.py
@@ -4,9 +4,9 @@ import MySQLdb
 import simplejson
 import pyowm
 import urllib
+import json
 
 app = Flask(__name__)
-conn = MySQLdb.connect(host = "localhost", user = "eis", passwd = "188esplanade", db="expertsystem")
 
 googleapikey = 'AIzaSyBaIP0AbBO1GgS8q9i28FQGd8HopCMNES0'
 weatherapikey = 'cdb2f4f903237bca966049d773d2e06e'
@@ -16,8 +16,8 @@ def create_task():
     if not request.json or not 'gps' in request.json or not 'phone' in request.json or not 'pests' in request.json or not 'aid' in request.json or not 'mac' in request.json or not 'serial' in request.json or not 'time' in request.json:
         abort(400)
 
-    latitude = '49.2324'#request.json['gps'][0]['latitude']
-    longitude = '-123.231'#request.json['gps'][0]['longitude']
+    latitude = str(json.loads(request.json['gps'])['latitude'])
+    longitude = str(json.loads(request.json['gps'])['longitude'])
 
     url = 'https://maps.googleapis.com/maps/api/elevation/json?locations='+str(latitude)+','+str(longitude)+'&key=AIzaSyBaIP0AbBO1GgS8q9i28FQGd8HopCMNES0'
     resp = simplejson.load(urllib.urlopen(url))
@@ -51,16 +51,16 @@ def create_task():
         'clouds': clouds
     }
 
-    x=conn.cursor()
-
     try:
+        conn = MySQLdb.connect(host = "localhost", user = "eis", passwd = "188esplanade", db="expertsystem")
+        x=conn.cursor()
         x.execute("""INSERT INTO fia(phone, gps, pests, aid, mac, serial, time, elevation, windspeed, winddir, humidity, temp, clouds) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (data['phone'],data['gps'], data['pests'], data['aid'], data['mac'], data['serial'], data['time'], elevation, windspeed, winddir, humidity, temp, clouds))
         conn.commit()
     except Exception, Argument:
         print Argument
         conn.rollback()
-
-    conn.close()
+    finally:
+        conn.close()
     
     return jsonify({'data':data}),201 
 
